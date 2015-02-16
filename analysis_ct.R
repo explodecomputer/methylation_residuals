c75405ff3f4ac87a1b3e05f1df79ba11a84186b0
Gibran Hemani <explodecomputer@gmail.com>
2015-02-16 18:02:55 +0000

    performed analysis that shows decreasing h2 as age increases

---
+++ analysis_ct.R
@@ -0,0 +1,74 @@
+# objectives:
+
+# what is the average snp heritability
+# - cis / trans
+# how does this change with age
+# what proportion of estimated genetic variation is captured by SNPs
+
+# is there a difference in SNP heritability for different genomic features
+
+
+
+
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+
+
+load("~/repo/methylation_residuals/data/datl_v1.RData")
+
+# total h2 divided into cis and trans for each time point
+
+with(datl, tapply(value, list(timepoint, variable, experiment), mean))
+
+p1 <- group_by(datl, timepoint, variable, experiment)
+p1 <- summarise(p1, ave = mean(value), med=median(value), se = sqrt(var(value) / length(value)), c05 = quantile(value, c(0.25)), c95 = quantile(value, 0.75))
+
+# ggplot(subset(p1, experiment=="SNP heritability" & variable != "Total"), aes(y=ave, x=timepoint)) +
+# geom_bar(stat="identity", aes(fill=variable)) +
+# labs(y="SNP heritability", fill="Variance\ncomponent", x="")
+
+
+# proportion explained h2 divided into cis and trans
+
+fr <- subset(p1, experiment=="mQTL")
+fr$prop <- subset(p1, experiment=="mQTL")$ave / subset(p1, experiment=="SNP heritability")$ave
+fr$ave <- subset(p1, experiment=="SNP heritability")$ave - subset(p1, experiment=="mQTL")$ave
+temp <- subset(p1, variable != "Total" & experiment == "mQTL", select=c(timepoint, variable, ave))
+fr <- subset(fr, variable != "Total", select=c(timepoint, variable, ave))
+fr$expl <- "Unexplained"
+temp$expl <- "Explained"
+
+fr <- rbind(fr, temp)
+fr$lab <- factor(paste(fr$variable, fr$expl))
+fr$lab <- factor(fr$lab, levels=levels(fr$lab)[c(2,1,4,3)])
+
+
+ggplot(fr, aes(y=ave, x=timepoint, order=order(timepoint))) +
+geom_bar(stat="identity", aes(fill=lab, order=order(lab))) +
+scale_fill_brewer(type="qual", palette=3) +
+labs(y="SNP heritability", x="", fill="")
+ggsave(file="~/repo/methylation_residuals/images/h2_partition_cistrans.pdf")
+
+
+
+# is there a difference in SNP heritability for different genomic features
+
+summary(lm(value ~ Feature + timepoint, subset(datl, variable=="Total"&experiment=="SNP heritability")))
+summary(lm(value ~ Feature + timepoint, subset(datl, variable=="Cis"&experiment=="SNP heritability")))
+summary(lm(value ~ Feature * as.numeric(timepoint), subset(datl, variable=="Cis"&experiment=="SNP heritability")))
+summary(lm(value ~ Feature * timepoint, subset(datl, variable=="Cis"&experiment=="SNP heritability")))
+# No interaction between h2 and time point, just take childhood for simplicity
+
+
+p2 <- group_by(subset(datl, experiment == "SNP heritability" & timepoint == "Childhood" & variable == "Cis"), Feature)
+p2 <- summarise(p2, ave = mean(value), med=median(value), se = sqrt(var(value) / length(value)), c05 = quantile(value, c(0.25)), c95 = quantile(value, 0.75))
+p2$Feature <- factor(p2$Feature, levels=levels(p2$Feature)[order(p2$ave)])
+
+ggplot(p2, aes(y=ave, x=Feature)) +
+geom_bar(stat="identity") +
+geom_errorbar(aes(ymax=ave+se, ymin=ave-se, width=0.2)) +
+geom_hline(data=subset(p1, experiment=="SNP heritability" & timepoint == "Childhood" & variable == "Cis"), aes(yintercept=ave)) +
+theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5), axis.ticks.x=element_blank()) +
+labs(x="", y="Cis SNP heritability")
+ggsave(file="~/repo/methylation_residuals/images/cis_h2_features.pdf")
--- calc_varexp.R
+++ calc_varexp.R
@@ -1,8 +1,6 @@
-library(plyr)
+library(dplyr)
+library(reshape2)
 
-wd <- "~/repo/mQTL-partitioning/filter_run5_gwas/data"
-timepoint <- c("15up", "antenatal", "cord", "F7", "FOM")
-filename <- file.path(wd, paste(timepoint, ".txt", sep=""))
 
 calcHsq <- function(pval, n)
 {
@@ -26,6 +24,50 @@ inferInterpolate <- function(pval, mod)
 	predict(mod, new)
 }
 
+harmoniseDat <- function(qd, dat, annot)
+{
+	levels(dat$timepoint) <- c("Birth", "Childhood", "Pregnancy", "Middle age", "Adolescence")
+	qd$timepoint <- as.factor(qd$timepoint)
+	levels(qd$timepoint) <- c("Adolescence", "Pregnancy", "Birth", "Childhood", "Middle age")
+	dat$hsq <- dat$hsq1 + dat$hsq2
+	dat2 <- melt(subset(dat, select=c(CPG, timepoint, hsq, hsq1, hsq2)), id.vars=c("CPG", "timepoint"))
+	names(qd) <- names(dat2)
+
+	qd$experiment <- "mQTL"
+	dat2$experiment <- "SNP heritability"
+
+	datl <- rbind(dat2, qd)
+	levels(datl$variable) <- c("Total", "Cis", "Trans")
+	datl$timepoint <- factor(datl$timepoint, levels=c("Birth", "Childhood", "Adolescence", "Pregnancy", "Middle age"))
+
+	# Put 0s for cpgs that have no mqtl
+
+	datl$code <- with(datl, paste(CPG, timepoint, variable))
+	a <- subset(datl, experiment=="SNP heritability")
+	b <- subset(datl, experiment=="mQTL", select=c(value, experiment, code))
+	ab <- merge(a, b, by="code", all.x=TRUE)
+	ab$value.y[is.na(ab$value.y)] <- 0
+
+	b <- subset(ab, select=c(CPG, timepoint, variable, value.y))
+	a <- subset(a, select=-c(code))
+	b$experiment <- "mQTL"
+	names(b) <- names(a)
+	datl <- rbind(a, b)
+
+	datl <- merge(datl, annot, by="CPG", all.x=TRUE)
+	levels(datl$Feature) <- c("3' UTR", "5' UTR", "Coding", "Downstream", "Essential splice site", "Intergenic", "Intronic", "Regulatory region", "Splice site", "Upstream", "Within mature miRNA", "Within non coding gene")
+	datl$Feature[datl$Feature=="Essential splice site"] <- "Splice site"
+	datl$Feature[datl$Feature=="Within mature miRNA"] <- "Within non coding gene"
+	datl$Feature <- factor(datl$Feature)
+
+	return(datl)
+}
+
+
+wd <- "~/repo/mQTL-partitioning/filter_run5_gwas/data"
+timepoint <- c("15up", "antenatal", "cord", "F7", "FOM")
+filename <- file.path(wd, paste(timepoint, ".txt", sep=""))
+
 
 b <- list()
 for(i in 1:length(timepoint))
@@ -39,9 +81,32 @@ for(i in 1:length(timepoint))
 	b[[i]] <- a
 }
 
-qtldat <- rbind.fill(b)
+qtldat <- rbind_all(b)
 index <- which(is.na(qtldat$varexp))
 mod <- interpolateMod(800)
 qtldat$varexp[index] <- inferInterpolate(qtldat$pval[index], mod)
 
-save(qtldat, file="~/repo/methylation_residuals/data/varexp.RData")
+qtldat <- subset(qtldat, select=c(CPG, timepoint, Trans, varexp))
+levels(qtldat$Trans) <- c("hsq1", "hsq2")
+
+qd <- group_by(qtldat, CPG, timepoint, Trans)
+qd <- summarise(qd, varexp=sum(varexp))
+
+qd2 <- group_by(qd, CPG, timepoint)
+qd2 <- summarise(qd2, varexp=sum(varexp))
+
+qd2$Trans <- "hsq"
+
+qd2 <- subset(qd2, select=c(CPG, timepoint, Trans, varexp))
+qd <- rbind(qd, qd2)
+
+
+load("~/repo/methylation_residuals/hsq_ct_aries.RData")
+annot <- read.table("~/repo/methylation_residuals/data/annot.txt", sep="\t")
+annot <- subset(annot, select=c(V1, V8))
+names(annot) <- c("CPG", "Feature")
+
+
+datl <- harmoniseDat(qd, dat)
+save(datl, file="~/repo/methylation_residuals/data/datl_v1.RData")
+save(qd, file="~/repo/methylation_residuals/data/varexp.RData")
